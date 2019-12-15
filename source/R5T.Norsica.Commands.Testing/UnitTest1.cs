e3448a96e2dbd944a925bd0f3f68ef078438fe31
MinexAutomation <minex.automation@gmail.com>
2019-12-14 21:51:28 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Norsica.Commands.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Norsica.Commands.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
